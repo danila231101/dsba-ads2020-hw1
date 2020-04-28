b687b1b1ab46ab5157a52204aeb63cf0c7d11747
danila231101 <63615581+danila231101@users.noreply.github.com>
2020-04-28 19:23:18 +0300

    Add files via upload

---
+++ src/graph.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv("/Users/danilakokin/Desktop/graph_results.csv")
+
+fig = plt.figure()
+ax = plt.axes()
+ax.plot('length', 'GradeSchool', data = df, label = "GradeSchool", color='blue')
+ax.plot('length', 'Karatsuba', data = df, label = "Karatsuba", color='purple')
+ax.plot('length', 'Divivde&Conquer', data = df, label = "Divide & Conquere", color='red')
+
+plt.title("Algorithm trends")
+plt.xlabel("Number of Digits")
+plt.ylabel("Seconds")
+
+plt.legend()
+plt.show()
